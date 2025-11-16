ed5d036c3151581e2f253b2ea5aca874fe3af0c6
Roody <roodylovesetihad@gmail.com>
2025-11-16 02:12:26 +0000

    Final stage of ECS Project

--- infra/modules/codedeploy/main.tf
+++ infra/modules/codedeploy/main.tf
@@ -76,7 +76,7 @@ resource "aws_codedeploy_deployment_group" "main" {
   load_balancer_info {
     target_group_pair_info {
       target_group {
-        name = var.blue_target_group_name
+      name = var.blue_target_group_name
       }
       target_group {
         name = var.green_target_group_name
