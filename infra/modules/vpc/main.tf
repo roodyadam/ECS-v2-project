7dd10a8ab4dabf0da58cd3d7e7e83671ade3120d
Roody <roodylovesetihad@gmail.com>
2025-11-22 03:39:28 +0000

    final test [skip ci]

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -49,14 +49,15 @@ resource "aws_subnet" "public" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
   tags = {
     Name = "${var.project_name}-public-rt"
   }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_route_table_association" "public" {
